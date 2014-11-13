1182bbbee855494b814dacf41c9e342928554d96
aa65535 <aa65535@live.com>
2014-11-13 19:07:58 +0800

    update shadowsocks luasrc

--- files/luci/model/cbi/shadowsocks.lua
+++ files/luci/model/cbi/shadowsocks.lua
@@ -15,6 +15,7 @@ general = m:section(TypedSection, "shadowsocks", translate("General Setting"))
 general.anonymous = true
 
 enable = general:option(Flag, "enable", translate("Enable"))
+enable.default = 1
 enable.rmempty = false
 
 use_conf_file = general:option(Flag, "use_conf_file", translate("Use Config File"))
@@ -22,26 +23,21 @@ use_conf_file.default = 1
 use_conf_file.rmempty = false
 
 config_file = general:option(Value, "config_file", translate("Config File"))
-config_file.default = "/etc/shadowsocks/config.json"
 config_file.placeholder = "/etc/shadowsocks/config.json"
 config_file.datatype = "file"
-config_file.optional = false
 config_file:depends("use_conf_file", 1)
 
 server = general:option(Value, "server", translate("Server Address"))
 server.datatype = "host"
-server.optional = false
 server:depends("use_conf_file", "")
 
 server_port = general:option(Value, "server_port", translate("Server Port"))
 server_port.datatype = "port"
-server_port.optional = false
 server_port:depends("use_conf_file", "")
 
 local_port = general:option(Value, "local_port", translate("Local Port"))
 local_port.datatype = "port"
 local_port.placeholder = 1080
-local_port.optional = false
 local_port:depends("use_conf_file", "")
 
 password = general:option(Value, "password", translate("Password"))
@@ -69,7 +65,6 @@ encrypt_method:depends("use_conf_file", "")
 ignore_list = general:option(Value, "ignore_list", translate("Ignore IP List"))
 ignore_list.placeholder = "/etc/shadowsocks/ignore.list"
 ignore_list.datatype = "file"
-ignore_list.optional = false
 
 
 tunnel = m:section(TypedSection, "shadowsocks", translate("UDP Forward"))
@@ -81,18 +76,12 @@ tunnel_enable.rmempty = false
 
 tunnel_port = tunnel:option(Value, "tunnel_port", translate("UDP Local Port"))
 tunnel_port.datatype = "port"
-tunnel_port.default = 5353
 tunnel_port.placeholder = 5353
-tunnel_port.optional = false
-tunnel_port:depends("tunnel_enable", 1)
 
 tunnel_forward = tunnel:option(Value, "tunnel_forward",
 	translate("Forwarding Tunnel"),
 	translate("Setup a local port forwarding tunnel"))
-tunnel_forward.default = "8.8.4.4:53"
 tunnel_forward.placeholder = "8.8.4.4:53"
-tunnel_forward.optional = false
-tunnel_forward:depends("tunnel_enable", 1)
 
 
 ac = m:section(TypedSection, "shadowsocks", translate("Access Control"))
@@ -102,19 +91,19 @@ ac_mode = ac:option(ListValue, "ac_mode", translate("Access Control Mode"))
 ac_mode:value("0", translate("Off"))
 ac_mode:value("1", translate("Accept"))
 ac_mode:value("2", translate("Reject"))
+ac_mode.default = 0
+ac_mode.rmempty = false
 
 accept_ip = ac:option(DynamicList, "accept_ip",
 	translate("Accept IP"),
 	translate("The traffic of these IP will be transited through shadowsocks"))
 accept_ip.datatype = "ipaddr"
-accept_ip.optional = false
 accept_ip:depends("ac_mode", 1)
 
 reject_ip = ac:option(Value, "reject_ip",
 	translate("Reject IP"),
 	translate("The traffic of this IP won't be transited through shadowsocks"))
 reject_ip.datatype = "ipaddr"
-reject_ip.optional = false
 reject_ip:depends("ac_mode", 2)
 
 return m
