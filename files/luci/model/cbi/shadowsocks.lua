6cf9691176865d5a7383a131be9219edc4bec4f7
Jian Chang <aa65535@live.com>
2016-08-09 19:45:31 +0800

    refine interface select

--- files/luci/model/cbi/shadowsocks.lua
+++ files/luci/model/cbi/shadowsocks.lua
@@ -8,6 +8,8 @@ local m, s, o
 local shadowsocks = "shadowsocks"
 local uci = luci.model.uci.cursor()
 local ipkg = require("luci.model.ipkg")
+local nwm = require("luci.model.network").init()
+local fwm = require("luci.model.firewall").init()
 
 function is_running(name)
 	return luci.sys.call("pidof %s >/dev/null" %{name}) == 0
@@ -175,7 +177,7 @@ s = m:section(TypedSection, "access_control", translate("Access Control"))
 s.anonymous = true
 
 -- Part of WAN
-s:tab("wan_ac", translate("Interfaces - WAN"))
+s:tab("wan_ac", translate("Zone WAN"))
 
 o = s:taboption("wan_ac", Value, "wan_bp_list", translate("Bypassed IP List"))
 o:value("/dev/null", translate("NULL - As Global Proxy"))
@@ -190,21 +192,23 @@ o = s:taboption("wan_ac", DynamicList, "wan_fw_ips", translate("Forwarded IP"))
 o.datatype = "ip4addr"
 
 -- Part of LAN
-s:tab("lan_ac", translate("Interfaces - LAN"))
+s:tab("lan_ac", translate("Zone LAN"))
 
-o = s:taboption("lan_ac", DynamicList, "interface", translate("Interface"))
-o.template = "cbi/network_netlist"
-o.nocreate = true
-o.unspecified = false
-o.widget = "checkbox"
-o.default = "lan"
-o.rmempty = false
+o = s:taboption("lan_ac", MultiValue, "lan_ifaces", translate("Interface"))
+for _, zone in ipairs(fwm:get_zones()) do
+	if string.find(zone:name(), "wan") ~= 1 then
+		local net = nwm:get_network(zone:name())
+		local device = net and net:get_interface()
+		if device then
+			o:value(device:name(), device:get_i18n())
+		end
+	end
+end
 
 o = s:taboption("lan_ac", ListValue, "lan_default_target", translate("Default Action"))
 o:value("SS_SPEC_WAN_AC", translate("Normal"))
 o:value("RETURN", translate("Bypassed"))
 o:value("SS_SPEC_WAN_FW", translate("Global"))
-o.default = "SS_SPEC_WAN_AC"
 
 o = s:taboption("lan_ac", DynamicList, "lan_hosts_action", translate("Hosts Action"))
 for _, v in ipairs(arp_table) do
