a86492848469bda5c3b6e872b140b23c5646475e
Jian Chang <aa65535@live.com>
2016-09-12 10:14:05 +0800

    refine code

--- files/luci/model/cbi/chinadns.lua
+++ files/luci/model/cbi/chinadns.lua
@@ -5,39 +5,39 @@
 local m, s, o
 
 if luci.sys.call("pidof chinadns >/dev/null") == 0 then
-	m = Map("chinadns", translate("ChinaDNS"), translate("ChinaDNS is running"))
+	m = Map("chinadns", translate("ChinaDNS"), "%s - %s" %{translate("ChinaDNS"), translate("RUNNING")})
 else
-	m = Map("chinadns", translate("ChinaDNS"), translate("ChinaDNS is not running"))
+	m = Map("chinadns", translate("ChinaDNS"), "%s - %s" %{translate("ChinaDNS"), translate("NOT RUNNING")})
 end
 
 s = m:section(TypedSection, "chinadns", translate("General Setting"))
-s.anonymous = true
+s.anonymous   = true
 
 o = s:option(Flag, "enable", translate("Enable"))
-o.rmempty = false
+o.rmempty     = false
 
 o = s:option(Flag, "bidirectional",
 	translate("Enable Bidirectional Filter"),
 	translate("Also filter results inside China from foreign DNS servers"))
-o.rmempty = false
+o.rmempty     = false
 
 o = s:option(Value, "port", translate("Local Port"))
 o.placeholder = 5353
-o.default = 5353
-o.datatype = "port"
-o.rmempty = false
+o.default     = 5353
+o.datatype    = "port"
+o.rmempty     = false
 
 o = s:option(Value, "chnroute", translate("CHNRoute File"))
 o.placeholder = "/etc/chinadns_chnroute.txt"
-o.default = "/etc/chinadns_chnroute.txt"
-o.datatype = "file"
-o.rmempty = false
+o.default     = "/etc/chinadns_chnroute.txt"
+o.datatype    = "file"
+o.rmempty     = false
 
 o = s:option(Value, "server",
 	translate("Upstream Servers"),
 	translate("Use commas to separate multiple ip address"))
 o.placeholder = "114.114.114.114,208.67.222.222:443,8.8.8.8"
-o.default = "114.114.114.114,208.67.222.222:443,8.8.8.8"
-o.rmempty = false
+o.default     = "114.114.114.114,208.67.222.222:443,8.8.8.8"
+o.rmempty     = false
 
 return m
--- files/luci/model/cbi/shadowvpn.lua
+++ files/luci/model/cbi/shadowvpn.lua
@@ -5,77 +5,68 @@
 local m, s, o
 
 if luci.sys.call("pidof shadowvpn >/dev/null") == 0 then
-	m = Map("shadowvpn", translate("ShadowVPN"), translate("ShadowVPN is running"))
+	m = Map("shadowvpn", translate("ShadowVPN"), "%s - %s" %{translate("ShadowVPN"), translate("RUNNING")})
 else
-	m = Map("shadowvpn", translate("ShadowVPN"), translate("ShadowVPN is not running"))
+	m = Map("shadowvpn", translate("ShadowVPN"), "%s - %s" %{translate("ShadowVPN"), translate("NOT RUNNING")})
 end
 
 -- General Setting
 s = m:section(TypedSection, "shadowvpn", translate("General Setting"))
-s.anonymous = true
+s.anonymous   = true
 
 o = s:option(Flag, "enable", translate("Enable"))
-o.default = 1
-o.rmempty = false
+o.default     = 1
+o.rmempty     = false
 
 o = s:option(Value, "server", translate("Server Address"))
-o.datatype = "host"
-o.rmempty = false
+o.datatype    = "host"
+o.rmempty     = false
 
 o = s:option(Value, "port", translate("Server Port"))
-o.datatype = "port"
-o.rmempty = false
+o.datatype    = "port"
+o.rmempty     = false
 
 o = s:option(Value, "user_token", translate("User Token"))
 o:value("0000000000000000", translate("Disabled"))
-o.default = "0000000000000000"
-o.datatype = "rangelength(16,16)"
-o.rmempty = false
+o.default     = "0000000000000000"
+o.datatype    = "rangelength(16,16)"
+o.rmempty     = false
 
 o = s:option(Value, "password", translate("Password"))
-o.password = true
-o.rmempty = false
-
---[[
-o = s:option(Value, "concurrency",
-	translate("Concurrency Number"),
-	translate("Must be the same value with server"))
-o.default = 1
-o.datatype = "uinteger"
-o.rmempty = false
---]]
+o.password    = true
+o.rmempty     = false
 
 o = s:option(Value, "net", translate("Local Subnet"))
 o.placeholder = "10.7.0.2/24"
-o.default = "10.7.0.2/24"
-o.datatype = "ip4addr"
-o.rmempty = false
+o.default     = "10.7.0.2/24"
+o.datatype    = "ip4addr"
+o.rmempty     = false
 
 o = s:option(Value, "mtu", translate("Override MTU"))
 o.placeholder = 1440
-o.default = 1440
-o.datatype = "range(296,1500)"
-o.rmempty = false
+o.default     = 1440
+o.datatype    = "range(296,1500)"
+o.rmempty     = false
 
 o = s:option(Value, "intf", translate("Interface Name"))
 o.placeholder = "tun0"
-o.default = "tun0"
-o.rmempty = false
+o.default     = "tun0"
+o.rmempty     = false
 
 -- Route Configuration
 s = m:section(TypedSection, "shadowvpn", translate("Routing Configuration"))
-s.anonymous = true
+s.anonymous   = true
 
 o = s:option(ListValue, "route_mode", translate("Routing Mode"))
 o:value("0", translate("Global Mode"))
 o:value("1", translate("Domestic Routes"))
 o:value("2", translate("Foreign Routes"))
-o.default = 0
-o.rmempty = false
+o.default     = "0"
+o.rmempty     = false
 
 o = s:option(Value, "route_file", translate("Routing File"))
-o.datatype = "file"
-o:depends("route_mode", 1)
-o:depends("route_mode", 2)
+o.datatype    = "file"
+o:depends("route_mode", "1")
+o:depends("route_mode", "2")
 
 return m
