095269cff7d47595e9af9ec2893f3510ea7efd47
Jian Chang <aa65535@live.com>
2018-05-18 23:20:06 +0800

    roll up year to 2018
    Signed-off-by: Jian Chang <aa65535@live.com>

--- files/luci/model/cbi/dns-forwarder.lua
+++ files/luci/model/cbi/dns-forwarder.lua
@@ -1,5 +1,5 @@
--- Copyright (C) 2016 OpenWrt-dist
--- Copyright (C) 2016 Jian Chang <aa65535@live.com>
+-- Copyright (C) 2016-2018 OpenWrt-dist
+-- Copyright (C) 2016-2018 Jian Chang <aa65535@live.com>
 -- Licensed to the public under the GNU General Public License v3.
 
 local m, s, o
